3566b82b04dd5a8e331da22cf41cb2d399e5ac5d
Aravind V V <arvndvv@gmail.com>
2020-12-31 15:09:28 +0530

    connected to backend from https://github.com/arvndvv/TodoBackend.git

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -16,16 +16,19 @@ todos:Todo[];
    this.todoService.getTodos().subscribe(
      todos=>{
        this.todos=todos;
+      //  console.log(todos)
+    //  this.todos.forEach(todo=>  console.log(todo.completed));
+      
      }
    );
   }
 deleteTodo(todo:Todo){
   this.todoService.deleteTodo(todo).subscribe(()=>{
   
-  this.todos=this.todos.filter(t=>t.id!==todo.id);
+  this.todos=this.todos.filter(t=>t._id!==todo._id);
 
   });
-  console.log(todo)
+  // console.log(todo)
 }
 addTodo(todo:any){
   this.todoService.addTodo(todo).subscribe(todo=>{
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -11,28 +11,28 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class TodoService {
-  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
-  limit = '?_limit=5';
+  todosUrl: string = 'http://localhost:4201';
+  
   constructor(private http: HttpClient) {}
 
   // get all todos
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(`${this.todosUrl}${this.limit}`);
+    return this.http.get<Todo[]>(`${this.todosUrl}`);
   }
 
   // create new todo
 
   addTodo(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+    return this.http.post<Todo>(`${this.todosUrl}/add`, todo, httpOptions);
   }
   // mark as completed
   toggleCompleted(todo: Todo): Observable<any> {
-    const url = `${this.todosUrl}/${todo.id}`;
+    const url = `${this.todosUrl}/completed/${todo._id}`;
     return this.http.put(url, todo, httpOptions);
   }
   //delete todo
   deleteTodo(todo: Todo): Observable<Todo> {
-    const url = `${this.todosUrl}/${todo.id}`;
+    const url = `${this.todosUrl}/remove/${todo._id}`;
     return this.http.delete<Todo>(url, httpOptions);
   }
 }
